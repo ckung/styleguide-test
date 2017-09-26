926377740e3a44dbcdc7fd019d433771482cfb17
Christina Kung <blurei@gmail.com>
2017-09-25 23:12:28 -0400

    Use includePaths node_modules

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,7 +6,14 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+    sassOptions: {
+      includePaths: [
+        'node_modules'
+      ]
+    }
   });
+
+  //app.import('node_modules/breakpoint-sass/stylesheets/_breakpoint.scss');
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
